0407321e33739b4b9dbfde116280b9f0522d2d6b
Jeff Nyman <jeffnyman@gmail.com>
2022-10-20 11:39:46 -0500

    Validating canvas capabilities.

--- src/assets/js/engine.js
+++ src/assets/js/engine.js
@@ -1,6 +1,32 @@
 function gameEngine() {
   this.init = function () {
     console.log("[DEBUG] Engine Started");
+
+    engine.validateCapabilities(function () {
+      engine.initEngine();
+    });
+  };
+
+  this.initEngine = function () {
+    console.log("[DEBUG] Initialize Engine");
+  };
+
+  this.validateCapabilities = function (good, bad) {
+    console.log("[DEBUG] Validating Requirements");
+
+    var allGood = true;
+
+    var c = document.createElement("canvas");
+    if (!(c.getContext && c.getContext("2d"))) {
+      allGood = false;
+    }
+
+    if (!allGood) {
+      console.log("[ERROR] Capabilities not validated.");
+      bad();
+    } else {
+      good();
+    }
   };
 }
 
